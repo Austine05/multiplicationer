5cd60834c2ede475047b0c42b1afcaeb493d85e0
Austine05 <mraustineterry@gmail.com>
2022-07-13 17:49:03 +0100

    added a new package: Multiply

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Austine05/multiplicationer
+
+go 1.18
---
+++ multiplicationer.go
@@ -0,0 +1,7 @@
+package multiplicationer
+
+import ()
+
+func Multiply(firstValue, secondValue float64) float64 {
+	return firstValue * secondValue
+}
